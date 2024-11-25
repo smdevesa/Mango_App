1ce9b83f34baebd77c7c5cc4825d9cb32234eacd
Santiago Devesa <sdevesa@itba.edu.ar>
2024-11-24 22:00:50 -0300

    fix: mango logo png

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,13 +14,13 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
+            isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
